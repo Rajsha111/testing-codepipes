7b85da3f57d2a9602dcadfd192c533ab4a765ffd
Rajsha111 <35475448+Rajsha111@users.noreply.github.com>
2021-03-12 17:41:45 +0530

    Update main.tf
    invalid TF added

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,21 +1,10 @@
 provider "aws" {
+  version = "~> 2.0"
   region  = "us-east-1"
 }
 
-provider "random" {}
-
-resource "random_string" "random" {
-  length    = 8
-  special   = false
-  min_lower = 8
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-047a51fa27710816e"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "testing-strato-${random_string.random.result}"
-  }
-
-  key_name ="qa-product"
+resource "aws_ebs_volume" "bad" {
+  availability_zone = "us-east-1a"
+  size              = 40
+  encrypted         = false
 }
